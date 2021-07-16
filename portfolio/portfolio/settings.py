c02f4c566b196e94ed59a71d32a67234db373075
sevilledev <sevillerasulova@gmail.com>
2021-07-16 19:22:55 +0400

    changed structure of portfolio

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8f1n!wxvlvw*hsnx=tgl4u)7&)okgl(mssv3%l9h-%)q&5y265'
+SECRET_KEY = 'django-insecure-_^z4q2_ztc*6+%83axa&y)^9k^k3kr%$s8j&ek+)m_yb_@dnyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
