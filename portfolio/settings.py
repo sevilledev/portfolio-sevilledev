0149452191415c6470b3e5b95fa3fec64834eed2
sevilledev <sevillerasulova@gmail.com>
2021-08-16 16:58:02 +0400

    static file storage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
